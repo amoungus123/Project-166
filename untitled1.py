dd6ac192de9496f60cb1ebda724d3d5717ba1034
amoungus123 <76024871+amoungus123@users.noreply.github.com>
2022-10-05 20:31:40 -0400

    Add files via upload

---
+++ untitled1.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct  5 20:02:00 2022
+
+@author: pulle
+"""
+
+from tkinter import*
+from PIL import ImageTk, Image
+
+root= Tk()
+root.title("Working On Canvas Using Functions")
+root.geometry("600x600")
+
+
+color_label=Label(root, text="Enter A Color: ")
+color_label.place(relx=0.6, rely=0.9, anchor= CENTER)
+
+input_box = Entry(root)
+input_box.insert(0, "black")
+
+
+
+canvas=Canvas(root, width = 590, height=510, bg = "white", highlightbackground="lightgrey")
+canvas.pack()
+
+
+img =ImageTk.PhotoImage(Image.open ("start_point1.png"))
+my_image = canvas.create_image(50,50,image=img)
+
+direction = ""
+oldx=50
+oldy=50
+newx=50
+newy=50
+
+def draw( direction,oldx,oldy,newx,newy):
+    fill_color = input_box.get()
+    
+
+root.bind("<Right>",right_dir)
+root.bind("<Left>",left_dir)
+root.bind("<Up>", up_dir)
+root.bind("<Down>",down_dir)
+
+root.mainloop()
